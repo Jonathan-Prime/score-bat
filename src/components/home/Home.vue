7febb06f6201c7703e3107d1cf4baa95be539069
Jonathan Molina <jonathan.molina@hrsolutions-co.com>
2021-10-27 19:01:11 -0500

    filter name leagues

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,15 +1,19 @@
 <template>
-    <div class="home-component">
-        <template v-if="lgs" >
-                <div class="card" v-for="(leagues, index) in lgs.slice(0, 5)" :key="index">
-                    <img class="card-img-top" :src="leagues.thumbnail" alt="Card image cap" style="width: 100%;">
-                    <div class="card-body">
-                        <h5 class="card-title" v-text="leagues.title"></h5>
-                        <p class="card-text" v-text="leagues.date"></p>
-                        <button type="button" class="btn btn-outline-primary" onclick="this.url">Live</button>
-                    </div>
+    <div>
+        <select class="select">
+                <option value=""><input type="text"></option>
+                <option v-for="league in searchResult" :key="league">{{ league.competition.name }}</option>
+        </select> 
+        <div class="home-component">
+            <div class="card" v-for="(leagues, index) in lgs.slice(0, 5)" :key="index">
+                <img class="card-img-top" :src="leagues.thumbnail" alt="Card image cap" style="width: 100%;">
+                <div class="card-body">
+                    <h5 class="card-title" v-text="leagues.title"></h5>
+                    <p class="card-text" v-text="leagues.date"></p>
+                    <button type="button" class="btn btn-outline-primary" onclick="this.url">Live</button>
                 </div>
-        </template>
+            </div>
+        </div>
     </div>
 </template>
 <script>    
@@ -17,11 +21,13 @@
 export default {
     name: 'HomeComponent',
     mounted() {
-        axios.get('https://www.scorebat.com/video-api/v1/')
+        this.created();
+    },
+    created(){
+        const api = 'https://www.scorebat.com/video-api/v1/';
+        axios.get(api)
             .then((rspt) => {
                 this.lgs = rspt.data;
-                this.url = rspt.data.url;
-                console.log(this.lgs);
                 return this.lgs.sort(function(){return 0.5 - Math.random()});
             }, Error => {
                 console.log(Error);
@@ -32,5 +38,18 @@ export default {
             lgs: [],
         }
     },
+   computed: {
+        searchResult() {
+        let lead = this.lgs;
+        
+        if (this.searchValue != '' && this.searchValue) {
+                lead = lead.filter((item) => {
+                    if (existing[item.competition.name]) return false;
+                    return existing[item.competition.name] = true;
+                })
+            }
+            return lead
+        }
+    }
 }
 </script>
